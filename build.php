9458cc28a988c0e4917a4a9e50353f52b766a036
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-10-23 21:31:29 -0500

    Initial commit of empty project structure.

---
+++ build.php
@@ -0,0 +1,26 @@
+#!/usr/bin/env php
+<?php
+require 'vendor/autoload.php';
+
+$phpcsCLI = new PHP_CodeSniffer_CLI();
+$phpcsViolations = $phpcsCLI->process(['standard' => ['PSR1'], 'files' => ['src', 'tests', 'build.php']]);
+if ($phpcsViolations > 0) {
+    exit(1);
+}
+
+$phpunitConfiguration = PHPUnit_Util_Configuration::getInstance(__DIR__ . '/phpunit.xml');
+$phpunitArguments = ['coverageHtml' => __DIR__ . '/coverage', 'configuration' => $phpunitConfiguration];
+$testRunner = new PHPUnit_TextUI_TestRunner();
+$result = $testRunner->doRun($phpunitConfiguration->getTestSuiteConfiguration(), $phpunitArguments);
+if (!$result->wasSuccessful()) {
+    exit(1);
+}
+
+$coverageFactory = new PHP_CodeCoverage_Report_Factory();
+$coverageReport = $coverageFactory->create($result->getCodeCoverage());
+if ($coverageReport->getNumExecutedLines() !== $coverageReport->getNumExecutableLines()) {
+    file_put_contents('php://stderr', "Code coverage was NOT 100%\n");
+    exit(1);
+}
+
+file_put_contents('php://stderr', "Code coverage was 100%\n");
